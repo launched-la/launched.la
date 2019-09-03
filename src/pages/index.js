e09b8e71e8abbdd5f4559052181c2727d144cd6c
Ethan Jon <ethanczahor@gmail.com>
2019-09-03 12:39:09 -0700

    added brand logos with links

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,9 +12,19 @@ export default ({data}) => <Page>
     <title>Launched LA</title>
   </Helmet>
 
-  <div style={{width: '400px'}}>
-    <Img fluid={data.file.childImageSharp.fluid} />
-    <p style={{marginTop: '2rem'}}>We conceive, explore, and launch healthy lifestyle next-gen brands offering premium but affordable choices in retail and beyond. We work with other passionate thought leaders to deliver on the promise that everyone deserves access to healthier alternatives.</p>
+  <div>
+    <Img fluid={data.logo.childImageSharp.fluid} style={{margin: '0 auto', maxWidth: '400px'}} />
+    <p style={{marginTop: '2rem', maxWidth: '700px'}}>We conceive, explore, and launch healthy lifestyle next-gen brands offering premium but affordable choices in retail and beyond. We work with other passionate thought leaders to deliver on the promise that everyone deserves access to healthier alternatives.</p>
+    <div className='grid'>
+      <a href='https://hellobello.com' target='_blank'>
+        <Img fluid={data.helloBelloLogo.childImageSharp.fluid} />
+      </a>
+      <a href='https://taffersmix.com' target='_blank'>
+        <Img fluid={data.taffersLogo.childImageSharp.fluid} />
+      </a>
+      <Img fluid={data.stateOfSunLogo.childImageSharp.fluid} />
+      <Img fluid={data.tillerAndHatchLogo.childImageSharp.fluid} />
+    </div>
     <p>For all inquiries please <a href='mailto:contact@launched.la'>contact us</a>.</p>
     <p>Los Angeles, USA // &copy;2019</p>
   </div>
@@ -22,7 +32,39 @@ export default ({data}) => <Page>
 
 export const query = graphql`
   query {
-    file(relativePath: { eq: "logo.png" }) {
+    logo: file(relativePath: { eq: "logo.png" }) {
+      childImageSharp {
+        fluid(maxWidth: 800) {
+          ...GatsbyImageSharpFluid_noBase64
+        }
+      }
+    }
+
+    helloBelloLogo: file(relativePath: { eq: "logo-hello-bello.png" }) {
+      childImageSharp {
+        fluid(maxWidth: 800) {
+          ...GatsbyImageSharpFluid_noBase64
+        }
+      }
+    }
+
+    stateOfSunLogo: file(relativePath: { eq: "logo-state-of-sun.png" }) {
+      childImageSharp {
+        fluid(maxWidth: 800) {
+          ...GatsbyImageSharpFluid_noBase64
+        }
+      }
+    }
+
+    taffersLogo: file(relativePath: { eq: "logo-taffers.png" }) {
+      childImageSharp {
+        fluid(maxWidth: 800) {
+          ...GatsbyImageSharpFluid_noBase64
+        }
+      }
+    }
+
+    tillerAndHatchLogo: file(relativePath: { eq: "logo-tiller-and-hatch.png" }) {
       childImageSharp {
         fluid(maxWidth: 800) {
           ...GatsbyImageSharpFluid_noBase64
