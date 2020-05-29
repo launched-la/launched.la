796c871d194307b8856b89d5580985b487e574e2
Ethan Jon <ethanczahor@gmail.com>
2020-05-29 13:44:26 -0700

    added link to careers page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,36 +1,48 @@
 import React from 'react'
-import {Helmet} from 'react-helmet'
-import {graphql} from 'gatsby'
+import { Helmet } from 'react-helmet'
+import { graphql } from 'gatsby'
 import Img from 'gatsby-image'
 
 import '../components/global.css'
 
 import Page from '../components/page'
 
-export default ({data}) => <Page>
-  <Helmet>
-    <title>Launched LA</title>
-  </Helmet>
+export default ({ data }) => (
+  <Page>
+    <Helmet>
+      <title>Launched LA</title>
+    </Helmet>
 
-  <div>
-    <Img fluid={data.logo.childImageSharp.fluid} style={{margin: '0 auto', maxWidth: '400px'}} />
-    <p style={{marginTop: '2rem', maxWidth: '700px'}}>We conceive, explore, and launch healthy lifestyle next-gen brands offering premium but affordable choices in retail and beyond. We work with other passionate thought leaders to deliver on the promise that everyone deserves access to healthier alternatives.</p>
-    <div className='grid'>
-      <a href='https://hellobello.com' target='_blank'>
-        <Img fluid={data.helloBelloLogo.childImageSharp.fluid} />
-      </a>
-      <a href='https://tillerandhatch.com' target='_blank'>
-        <Img fluid={data.tillerAndHatchLogo.childImageSharp.fluid} />
-      </a>
-      <a href='https://taffersmix.com' target='_blank'>
-        <Img fluid={data.taffersLogo.childImageSharp.fluid} />
-      </a>
-      <Img fluid={data.stateOfSunLogo.childImageSharp.fluid} />
+    <div>
+      <Img fluid={data.logo.childImageSharp.fluid} style={{ margin: '0 auto', maxWidth: '400px' }} />
+      <p style={{ marginTop: '2rem', maxWidth: '700px' }}>
+        We conceive, explore, and launch healthy lifestyle next-gen brands offering premium but affordable choices in retail and beyond. We
+        work with other passionate thought leaders to deliver on the promise that everyone deserves access to healthier alternatives.
+      </p>
+      <div className='grid'>
+        <a href='https://hellobello.com' target='_blank'>
+          <Img fluid={data.helloBelloLogo.childImageSharp.fluid} />
+        </a>
+        <a href='https://tillerandhatch.com' target='_blank'>
+          <Img fluid={data.tillerAndHatchLogo.childImageSharp.fluid} />
+        </a>
+        <a href='https://taffersmix.com' target='_blank'>
+          <Img fluid={data.taffersLogo.childImageSharp.fluid} />
+        </a>
+        <Img fluid={data.stateOfSunLogo.childImageSharp.fluid} />
+      </div>
+      <p>
+        <b>
+          Interested in joining our team? Check out our <a href='https://boards.greenhouse.io/launchedla'>Careers</a> page.
+        </b>
+      </p>
+      <p>
+        For all inquiries please <a href='mailto:contact@launched.la'>contact us</a>.
+      </p>
+      <p>17383 Suite B200 Sunset Blvd Pacific Palisades CA, 90272 USA // &copy;2019</p>
     </div>
-    <p>For all inquiries please <a href='mailto:contact@launched.la'>contact us</a>.</p>
-    <p>17383 Suite B200 Sunset Blvd Pacific Palisades CA, 90272 USA // &copy;2019</p>
-  </div>
-</Page>
+  </Page>
+)
 
 export const query = graphql`
   query {
